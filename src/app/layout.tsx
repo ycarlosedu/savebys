c87d69300afb8a9e857f7e192107b5870d1fcb4a
Carlos Eduardo Oliveira da Silva <silvacarlos02@sicredi.com.br>
2024-06-12 10:06:26 -0300

    feat: add new favicon and logos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { FloatingButton } from "@/components/FloatingButton";
+import { Toaster } from "@/components/Sonner";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
-import { Toaster } from "@/components/Sonner";
-import { FloatingButton } from "@/components/FloatingButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
