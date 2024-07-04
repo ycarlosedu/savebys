de57ce9e37bed481aa407ef643d15cf5ecb193c4
Carlos Silva <silvacarlosoliveira@outlook.com>
2024-07-04 14:51:00 -0300

    feat: add fecomercio description

--- src/app/fecomerciors/page.tsx
+++ src/app/fecomerciors/page.tsx
@@ -28,7 +28,7 @@ export default function MobiliaSolidaria() {
           <CaretRight size={16} />
         </Link>
       </div>
-      <section className="w-full max-w-default px-default py-16 flex flex-col gap-8 items-center">
+      <section className="w-full max-w-[900px] px-default py-16 flex flex-col gap-8 items-center">
         <h1 className="title">CAMPANHA MOBÍLIA SOLIDÁRIA</h1>
         <Image
           src="/images/campaigns/fecomerciors/banner-900x560.png"
@@ -36,6 +36,42 @@ export default function MobiliaSolidaria() {
           width={900}
           height={560}
         />
+        <div className="flex flex-col items-start gap-4 text-lg">
+          <p>
+            A Federação do Comércio de Bens e de Serviços do Estado do Rio
+            Grande do Sul (Fecomércio-RS) representa as empresas do comércio de
+            bens, serviços e turismo no Rio Grande do Sul. O Sistema
+            Fecomércio-RS conta com mais de 100 sindicatos empresariais em sua
+            base para subsidiar e intensificar suas ações. Gerencia ainda o
+            Sesc/RS e o Senac-RS, com mais de uma centena de Unidades
+            Operacionais no Estado, proporcionando ações na área de bem-estar
+            social e qualificação profissional a milhares de gaúchos.
+          </p>
+          <p>
+            Para contribuir com as vítimas das enchentes no Rio Grande do Sul
+            estamos promovendo uma campanha chamada Mobília Solidária que tem
+            como objetivo arrecadar e doar móveis para pessoas/empresas
+            atingidas com a catástrofe, através de um sindicato-curador, que irá
+            mapear e validar as doações.
+          </p>
+          <p>
+            A logística será de responsabilidade do próprio beneficiário que irá
+            fazer a retirada do seu móvel no local e hora combinada previamente.{" "}
+          </p>
+          <p className="font-bold">Regras para beneficiários:</p>
+          <ol className="list-decimal list-inside">
+            <li>Deverá estar dentro dos 95 municípios em calamidade.</li>
+            <li>Deverá fazer parte do CNAE do comércio.</li>
+          </ol>
+          <p className="font-bold">Regras para o doador:</p>
+          <ol className="list-decimal list-inside">
+            <li>Qualquer pessoa poderá doar seus móveis para a campanha.</li>
+            <li>
+              As fotos a serem cadastradas deverão estar nítidas e com no máximo
+              1Mb.
+            </li>
+          </ol>
+        </div>
       </section>
     </main>
   );
