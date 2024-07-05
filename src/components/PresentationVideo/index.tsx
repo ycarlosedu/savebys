df45adabf1794c0580aff91adcfce48485ac280c
Carlos Silva <silvacarlosoliveira@outlook.com>
2024-07-05 13:53:36 -0300

    feat: change video to youtube

--- src/components/PresentationVideo/index.tsx
+++ src/components/PresentationVideo/index.tsx
@@ -1,17 +1,17 @@
-import Script from "next/script";
-
 export default function PresentationVideo() {
   return (
     <section className="bg-gradientFormsToTop py-8 w-full flex flex-col items-center justify-center">
-      <Script src="https://player.vimeo.com/api/player.js" />
-
       <div className="max-w-default px-default w-full flex flex-col items-center justify-center">
         <div className="rounded-3xl overflow-hidden w-full">
           <iframe
-            src="https://player.vimeo.com/video/976569934?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-            allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
             className="w-full bg-white h-[300px] md:h-[639px]"
-            title="Apresentação da Empresa SaveBys"
+            width="639"
+            height="300"
+            src="https://www.youtube.com/embed/UtwtaZwC08M?si=HGP99Qw4f9BT7fkS"
+            title="SAVEBYS - Quem somos"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
           ></iframe>
         </div>
       </div>
