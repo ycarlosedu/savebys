abdd389daf0e521e184e3f814781c675336bac1d
Carlos Silva <silvacarlosoliveira@outlook.com>
2024-07-12 15:09:19 -0300

    feat: my bag badge count

--- src/components/fecomerciors/Header/index.tsx
+++ src/components/fecomerciors/Header/index.tsx
@@ -1,12 +1,16 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 
 import ActiveLink from "@/components/ActiveLink";
+import useProductStore from "@/stores/productStore";
 
 import { ShoppingBag } from "@phosphor-icons/react/dist/ssr";
 import Logo from "images/savebys/logo-savebys-gray-slogan-cropped.svg";
 
 export default function Header() {
+  const { products } = useProductStore();
+
   return (
     <header className="w-full bg-white flex flex-col lg:flex-row gap-6 items-center justify-center py-4 text-center text-gray-secondary flex-wrap z-10">
       <div className="w-full max-w-default flex flex-col lg:flex-row gap-6 items-center justify-between text-center flex-wrap px-default">
@@ -31,10 +35,15 @@ export default function Header() {
           </Link>
           <Link
             href="/fecomerciors/minha-sacola"
-            className="link-btn-secondary h-[54px]"
+            className="link-btn-secondary h-[54px] relative"
           >
             Minha Sacola
             <ShoppingBag size={32} />
+            {products.length > 0 && (
+              <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full w-6 h-6 flex items-center justify-center">
+                {products.length}
+              </span>
+            )}
           </Link>
         </nav>
       </div>
