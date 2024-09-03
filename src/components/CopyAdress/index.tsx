5dd8ba2d6dfc8edf3f7c41df5c8830acd8668fea
Carlos Eduardo Oliveira da Silva <carlos.oliveira@compasso.com.br>
2024-09-03 11:13:27 -0300

    fix: copy adress round style

--- src/components/CopyAdress/index.tsx
+++ src/components/CopyAdress/index.tsx
@@ -6,7 +6,7 @@ const OUR_ADRESS = "https://savebys.com";
 
 export default function CopyAdress() {
   return (
-    <fieldset className="flex w-full items-center border-2 border-gray-secondary rounded-xl overflow-hidden max-w-[360px] xs:min-w-[360px]">
+    <fieldset className="flex w-full items-center border-2 border-gray-secondary rounded-xl max-w-[360px] xs:min-w-[360px]">
       <input
         type="text"
         name="project-url"
@@ -17,7 +17,7 @@ export default function CopyAdress() {
       />
       <button
         type="submit"
-        className="link-btn h-[50px] px-8 w-full max-w-[124px] text-lg rounded-none"
+        className="link-btn h-[50px] px-8 w-full max-w-[124px] text-lg rounded-[10px]"
         onClick={() => handleCopyToClipboard(OUR_ADRESS)}
       >
         COPIAR
--- src/components/WhyChooseUs/index.tsx
+++ src/components/WhyChooseUs/index.tsx
@@ -3,7 +3,7 @@ export default function WhyChooseUs() {
     <section className="flex flex-col pb-16 px-default gap-8 max-w-[800px]">
       <h2 className="title">Por que escolher a SaveBys?</h2>
       <p className="text-xl md:text-3xl leading-10 text-center">
-        Nossos produtos são pautadas com curadoria para trazer maior segurança,
+        Nossos produtos são pautados com curadoria para trazer maior segurança,
         credibilidade e seriedade. O papel do Curador é fundamental para
         verificar in loco e validar as reais necessidades das empresas atingidas
         por catástrofes, trazendo maior transparência na gestão dos recursos.
