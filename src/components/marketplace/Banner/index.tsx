568d3b83e8fe0cef293e8fdbada762dc254f548d
Carlos Silva <silvacarlosoliveira@outlook.com>
2024-11-16 12:30:33 -0300

    feat: responsive marketplace banner

--- src/components/marketplace/Banner/index.tsx
+++ src/components/marketplace/Banner/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Banner() {
   return (
     <div className="w-full relative overflow-hidden rounded-[32px] py-8 px-8 flex flex-col justify-center gap-8">
-      <h2 className="text-5xl leading-normal font-bold text-white z-10">
+      <h2 className="text-2xl lg:text-5xl leading-normal font-bold text-white z-10">
         O seu primeiro <span className="text-primary">Marketplace</span>{" "}
         <br></br> de <span className="text-primary">Impacto Social</span>
       </h2>
@@ -16,7 +16,7 @@ export default function Banner() {
       </Link>
       <Image
         src="/images/marketplace/banner.png"
-        className="absolute inset-0 object-fill z-0"
+        className="absolute inset-0 object-cover object-center z-0 min-h-full"
         width={1200}
         height={300}
         alt="Banner do Marketplace da SaveBys"
